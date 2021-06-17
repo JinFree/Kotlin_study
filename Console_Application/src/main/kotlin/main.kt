497090f5a75d08e00de75cb82afd0a7919fb46c2
왕진영 <jinyeongwang@wangjin-yeong-ui-MacBookAir.local>
2021-06-17 23:58:21 +0900

    basic operations

--- Console_Application/src/main/kotlin/main.kt
+++ Console_Application/src/main/kotlin/main.kt
@@ -1,3 +1,24 @@
-fun main(args: Array<String>) {
+fun helloWorld(args: Array<String>) {
     println("Hello World!")
+}
+
+fun safeCallOperator(args: Array<String>)
+{
+    var c : String? = null
+    var len : Int? = c?.length
+    println(len)
+}
+
+fun elvisOperator(args: Array<String>)
+{
+    // ?: 연산
+    // A ?: B 일 때 A 가 null 이면 B 반환, null 이 아니면 A 반환
+    var one = null ?: 1
+    println(one)
+    var two = 2 ?: 1
+    println(two)
+}
+fun main(args: Array<String>)
+{
+    elvisOperator(args)
 }
